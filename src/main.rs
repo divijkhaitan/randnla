59b105a17d85ec2ce5819cf1dabbddd546731535
divijkhaitan <divijkhaitan@gmail.com>
2024-09-19 23:46:49 +0530

    Added code to sample sketching operators

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod sample;
 
-fn main(){
-    for i in 0..3 {
-        println!("{}", i);
-    }
+fn main() {
+    let n = 6;
+    let gaussian_matrix = sample::sketching_operator(sample::DistributionType::Gaussian, n, n);
+    println!("Gaussian Matrix:\n{}", gaussian_matrix);
+    
+    let uniform_matrix = sample::sketching_operator(sample::DistributionType::Uniform, n, n);
+    println!("Uniform Matrix:\n{}", uniform_matrix);
+    
+    let rademacher_matrix = sample::sketching_operator(sample::DistributionType::Rademacher, n, n);
+    println!("Rademacher Matrix:\n{}", rademacher_matrix);
 }
