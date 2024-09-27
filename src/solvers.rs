17264aa2c44d568903171c9c5e6f9ca42cf09e2d
divijkhaitan <divijkhaitan@gmail.com>
2024-09-28 00:01:13 +0530

    Added Least Sqaures SVD and Diagonal system solving

--- src/solvers.rs
+++ src/solvers.rs
@@ -10,12 +10,31 @@ where
     let mut x = DMatrix::zeros(n, 1);
     
     for i in (0..n).rev() {
-        let mut sum = T::zero();
-        for j in (i+1)..n {
-            sum += u[(i, j)] * x[(j, 0)];
+        if u[(i, i)] != T::zero()
+        {
+            let mut sum = T::zero();
+            for j in (i+1)..n {
+                sum += u[(i, j)] * x[(j, 0)];
+            }
+            
+            x[(i, 0)] = (y[(i, 0)] - sum) / u[(i, i)];
         }
-        
-        x[(i, 0)] = (y[(i, 0)] - sum) / u[(i, i)];
+    }
+    x
+}
+
+pub fn solve_diagonal_system<T>(u: &DMatrix<T>, y: &DMatrix<T>) -> DMatrix<T>
+where
+    T: Scalar + RealField + Zero + One + AddAssign + SubAssign + MulAssign + DivAssign + Copy,
+{
+    let n = u.nrows();
+    let mut x = DMatrix::zeros(n, 1);
+    
+    for i in (0..n).rev() {
+        if u[(i, i)] != T::zero()
+        {
+            x[(i, 0)] = y[(i, 0)] / u[(i, i)];
+        }
     }
     x
 }
