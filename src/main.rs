609a0b95269e4a19ca0bcaad9e48b94aa1a5dc34
divijkhaitan <divijkhaitan@gmail.com>
2024-10-03 21:40:48 +0530

    Finished merge

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-// mod sample;
+mod sample;
 mod cg;
 
 use nalgebra::{DMatrix, DVector};
 use rand::Rng;
 use std::time::Instant;
-use sketch::haar_sample;
 
-mod sketch;
-mod sketch_and_solve;
-mod solvers;
-mod sketch_and_precondition;
 fn main() {
     let n = 10; // Size of the problem
     // Generate a test problem
@@ -39,21 +34,5 @@ fn generate_test_problem(n: usize) -> (DMatrix<f64>, DVector<f64>) {
     let p: DMatrix<f64> = DMatrix::from_fn(n, n, |_, _| rng.gen_range(0.0..1.0));
     let a = &p.transpose() * &p;
     let b = DVector::from_element(n, 1.0);
-    let n = 6;
-    let row_haar_matrix = haar_sample(n, n, sketch::MatrixAttribute::Row);
-    println!("Row Orthogonal:\n{}", row_haar_matrix);
-    
-    let col_haar_matrix = haar_sample(n, n, sketch::MatrixAttribute::Column);
-    println!("Column Orthogonal:\n{}", col_haar_matrix);
-    
-    let gaussian_matrix = sketch::sketching_operator(sketch::DistributionType::Gaussian, n, n);
-    println!("Gaussian Matrix:\n{}", gaussian_matrix);
-    
-    let uniform_matrix = sketch::sketching_operator(sketch::DistributionType::Uniform, n, n);
-    println!("Uniform Matrix:\n{}", uniform_matrix);
-    
-    let rademacher_matrix = sketch::sketching_operator(sketch::DistributionType::Rademacher, n, n);
-    println!("Rademacher Matrix:\n{}", rademacher_matrix);
     (a, b)
-    
 }
