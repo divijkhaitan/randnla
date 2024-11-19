48e590c979ec3562e91ad87b616f23d6b4ae0645
SaptarishiD <saptarishi.dhanuka@gmail.com>
2024-11-20 00:53:21 +0530

    feat: added data aware sketching TSOG1

--- src/lora_helpers.rs
+++ src/lora_helpers.rs
@@ -60,6 +60,44 @@ pub fn RF1(A: &DMatrix<f64>, k: usize) -> DMatrix<f64> {
 // ======================================================================================
 // Tall Sketch Operator Generator
 
+pub fn tsog1(A: &DMatrix<f64>, k: usize, s: usize, num_passes: i32, passes_per_stab: i32 ) -> DMatrix<f64> {
+
+    let mut passes_done = 0;
+    let mut S: nalgebra::Matrix<f64, nalgebra::Dyn, nalgebra::Dyn, nalgebra::VecStorage<f64, nalgebra::Dyn, nalgebra::Dyn>> = DMatrix::from_element(1, 1, 0.0);
+    if num_passes % 2 == 0 {
+        S = sketch::sketching_operator(sketch::DistributionType::Gaussian, A.ncols(), k).unwrap();
+    }
+    else {
+        let pre_sketch = sketch::sketching_operator(sketch::DistributionType::Gaussian, A.nrows(), k).unwrap();
+
+        let mut S1 = A.transpose()*pre_sketch;
+        passes_done += 1;
+        if (passes_done % passes_per_stab == 0)
+        {
+            S = Stabilizer(&S1);
+        } 
+    }
+
+    let mut diff = num_passes - passes_done;
+
+    while diff >= 2 {
+        let S1 = A*&S;
+        passes_done += 1;
+        if (passes_done % passes_per_stab == 0)
+        {
+            S = Stabilizer(&S1);
+        } 
+        let S2 = A.transpose()*S1;
+        passes_done += 1;
+        if (passes_done % passes_per_stab == 0)
+        {
+            S = Stabilizer(&S2);
+        } 
+        diff -= 2;
+    }
+
+    return S;
+}
 
 
 
@@ -85,20 +123,11 @@ pub fn Orth(X: &DMatrix<f64>) -> DMatrix<f64> {
     X.clone().qr().q()
 }
 
-pub struct Stabilizer {
+
+pub fn Stabilizer(X: & DMatrix<f64>) -> DMatrix<f64> {
+    X.clone().qr().q()
 }
 
-impl Stabilizer {
-    pub fn new() -> Self {
-        Stabilizer {}
-    }
-
-    
-    pub fn stabilize(&self, y: &mut DMatrix<f64>) {
-        let qr = y.clone().qr();
-        *y = qr.q(); // we just need to output the q factor from the QR decomposition
-    }
-}
 // Orth methods
 // ====================================================================================
 
@@ -130,12 +159,30 @@ mod test_helpers
     use nalgebra::{DMatrix, DVector, dmatrix, dvector};
     use crate::lora_helpers;
     use crate::lora_drivers;
+    use crate::sketch;
     use rand_123::rng::ThreeFry2x64Rng;
     use rand_core::{SeedableRng, RngCore};
     use rand::Rng;
     use std::time::Instant;
     use rand_distr::{Distribution, Normal, Uniform, Bernoulli, StandardNormal};
     use rand::distributions::DistIter;
+
+
+    #[test]
+    fn test_tsog1_sketch() {
+
+        let mut rng_threefry = ThreeFry2x64Rng::seed_from_u64(0);
+        let normal = Normal::new(0.0, 1.0).unwrap();
+        let dims = 10;
+        let a =  DMatrix::from_fn(dims, dims, |_i, _j| normal.sample(&mut rng_threefry));
+        let k = 5;
+        let s = 0;
+        let num_passes = 1;
+        let passes_per_stab = 1;
+
+        let S = lora_helpers::tsog1(&a, k, s, num_passes, passes_per_stab);
+        println!("S: \n{}", S);
+    }
 
 
 }
